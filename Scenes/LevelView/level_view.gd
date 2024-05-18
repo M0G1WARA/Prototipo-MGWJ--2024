2a480931be63925b6709e1aafcbe23436dbb0ca5
M0G1WARA <121844050+M0G1WARA@users.noreply.github.com>
2024-05-17 19:28:51 -0600

    Create script level view

---
+++ Scenes/LevelView/level_view.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+var rotating = false
+
+var prev_mouse_position
+var next_mouse_position
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (Input.is_action_just_pressed("Rotate")):
+		rotating = true
+		prev_mouse_position = get_viewport().get_mouse_position()
+	if (Input.is_action_just_released("Rotate")):
+		rotating = false
+		
+	if rotating:
+		next_mouse_position = get_viewport().get_mouse_position()
+		$Level.rotate_y((next_mouse_position.x-prev_mouse_position.x)*.2*delta)
+		prev_mouse_position = next_mouse_position
